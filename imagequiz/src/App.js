574a7a3faa8b8460a3b4a2d3c09791733a3ec4ef
Noah <noahdrap@email.arizona.edu>
2021-02-24 11:00:56 -0700

    commit 3

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-class HelloMessage extends React.Component {
+class ShoppingList extends React.Component {
   render() {
     return (
-      <div>
-        Hello {this.props.name}
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
       </div>
     );
   }
 }
-
-ReactDOM.render(
-  <HelloMessage name="Taylor" />,
-  document.getElementById('hello-example')
-);
